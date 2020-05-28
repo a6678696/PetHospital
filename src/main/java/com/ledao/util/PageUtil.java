6ad088d5d6dd747b8d6c792e79a44a3aaa0d970d
LeDao <1203007469@qq.com>
2020-05-28 23:36:49 +0800

    LeDao宠物医院管理系统

--- src/main/java/com/ledao/util/PageUtil.java
+++ src/main/java/com/ledao/util/PageUtil.java
@@ -99,13 +99,14 @@ public class PageUtil {
     /**
      * 生成分页代码(查看评价专用)
      *
-     * @param targetUrl   目标地址
-     * @param totalNum    总记录数
-     * @param currentPage 当前页
-     * @param pageSize    每页大小
+     * @param targetUrl
+     * @param totalNum
+     * @param currentPage
+     * @param pageSize
+     * @param goodsId
      * @return
      */
-    public static String genPagination3(String targetUrl, long totalNum, int currentPage, int pageSize,int goodsId) {
+    public static String genPagination3(String targetUrl, long totalNum, int currentPage, int pageSize, int goodsId) {
         //当前页前后页的显示数量
         int pageTotal = 2;
         long totalPage = totalNum % pageSize == 0 ? totalNum / pageSize : totalNum / pageSize + 1;
@@ -113,9 +114,9 @@ public class PageUtil {
             return "未查询到数据";
         } else {
             StringBuffer pageCode = new StringBuffer();
-            pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + "1?goodsId="+goodsId + "' class='page-link'>首页</a></li>");
+            pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + "1?goodsId=" + goodsId + "' class='page-link'>首页</a></li>");
             if (currentPage > 1) {
-                pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + (currentPage - 1) + "' class='page-link'><span aria-hidden='true'>«</span><span class='sr-only'>Previous</span></a></li>");
+                pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + (currentPage - 1) + "?goodsId=" + goodsId + "' class='page-link'><span aria-hidden='true'>«</span><span class='sr-only'>Previous</span></a></li>");
             } else {
                 pageCode.append("<li class='disabled page-item'><a href='#' class='page-link'><span aria-hidden='true'>«</span><span class='sr-only'>Previous</span></a></li>");
             }
@@ -124,17 +125,17 @@ public class PageUtil {
                     continue;
                 }
                 if (i == currentPage) {
-                    pageCode.append("<li class='page-item active'><a href='" + targetUrl + "/" + i + "' class='page-link'>" + i + "</a></li>");
+                    pageCode.append("<li class='page-item active'><a href='" + targetUrl + "/" + i + "?goodsId=" + goodsId + "' class='page-link'>" + i + "</a></li>");
                 } else {
-                    pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + i + "' class='page-link'>" + i + "</a></li>");
+                    pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + i + "?goodsId=" + goodsId + "' class='page-link'>" + i + "</a></li>");
                 }
             }
             if (currentPage < totalPage) {
-                pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + (currentPage + 1) + "'  aria-label='Next' class='page-link'><span aria-hidden='true'>»</span><span class='sr-only'>Next</span></a></li>");
+                pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + (currentPage + 1) + "?goodsId=" + goodsId + "'  aria-label='Next' class='page-link'><span aria-hidden='true'>»</span><span class='sr-only'>Next</span></a></li>");
             } else {
                 pageCode.append("<li class='disabled page-item'><a href='#' aria-label='Next' class='page-link'><span aria-hidden='true'>»</span><span class='sr-only'>Next</span></a></li>");
             }
-            pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + totalPage + "' aria-label='Next' class='page-link'>尾页</a></li>");
+            pageCode.append("<li class='page-item'><a href='" + targetUrl + "/" + totalPage + "?goodsId=" + goodsId + "' aria-label='Next' class='page-link'>尾页</a></li>");
             return pageCode.toString();
         }
     }
