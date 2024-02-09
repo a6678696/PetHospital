ab36dc3eafe19cd14b4ed6358fe32110a340502a
a6678696 <1203007469@qq.com>
2024-02-09 22:13:55 +0800

    2024-02-09 22:13:47

--- src/main/java/com/ledao/PetHospitalApplication.java
+++ src/main/java/com/ledao/PetHospitalApplication.java
@@ -3,8 +3,6 @@ package com.ledao;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author LeDao
